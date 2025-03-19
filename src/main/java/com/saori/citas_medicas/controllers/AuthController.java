f81331969327c154946ca944ff96b152a9cee5f3
SaoriPLh <saojym12@gmail.com>
2025-03-19 02:55:31 -0600

    Uso de try Catch modificando cosas como la respuesta en el catch

--- src/main/java/com/saori/citas_medicas/controllers/AuthController.java
+++ src/main/java/com/saori/citas_medicas/controllers/AuthController.java
@@ -5,6 +5,12 @@ import com.saori.citas_medicas.dto.LoginRequest;
 import com.saori.citas_medicas.dto.RegistroRequest;
 import com.saori.citas_medicas.models.Usuario;
 import com.saori.citas_medicas.services.AuthService;
+import com.saori.citas_medicas.exceptions.AuthenticationFailedException;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +31,13 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        try{
+            
         String token = authService.authenticate(request.getEmail(), request.getPassword());
         return ResponseEntity.ok(new AuthResponse(token));
+        }catch(AuthenticationFailedException e){
+            AuthResponse respuesta = new AuthResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(respuesta);
+        }
     }
 }
