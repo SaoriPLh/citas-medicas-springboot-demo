660d51a538bf306c78d3f8b57bb1ded6bb8ebc6d
saorrimr <saojym12@gmail.com>
2025-04-05 23:04:49 -0600

    Manejo de estados en varios metodos para cambiar el estado en una cita

---
+++ src/main/java/com/saori/citas_medicas/services/EstadoCitaStrategy.java
@@ -0,0 +1,9 @@
+package com.saori.citas_medicas.services;
+
+import com.saori.citas_medicas.models.Cita;
+
+public interface EstadoCitaStrategy {
+
+    //solo modificara la cita entonces le pasamos la cita y void
+    void cambiarEstado(Cita cita);
+}
